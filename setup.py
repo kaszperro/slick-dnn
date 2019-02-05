c0e8a6b5a657df306db3a6a21036bfe57993fdae
Kasper - PC <kasper.sapala@gmail.com>
2019-02-05 01:23:10 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='slick-dnn',
+    version='',
+    packages=['slick_dnn', 'slick_dnn.data', 'slick_dnn.autograd'],
+    url='',
+    license='',
+    author='Kasper',
+    author_email='',
+    description=''
+)
