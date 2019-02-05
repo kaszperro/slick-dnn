87e526576f01eede1568a5e4dcaa44b88bfd0667
Kasper - PC <kasper.sapala@gmail.com>
2019-02-05 01:27:58 +0100

    docs init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
